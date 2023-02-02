5f8ee0344207f2b9ba591cd811109835b2578ee0
PaulHendi <paulhendi25@gmail.com>
2023-02-02 11:54:23 +0100

    Added FA2 smart contracts tuto

--- Increment/deploy_to_ghostnet.sh
+++ Increment/deploy_to_ghostnet.sh
@@ -1,6 +1,10 @@
 octez-client --endpoint https://rpc.ghostnet.teztnets.xyz config update
-octez-client gen keys alice
+octez-client import secret key owner SECRET_KEY
 octez-client originate contract increment \
-              transferring 0 from alice \
+              transferring 0 from owner \
               running increment.tz \
               --init 10 --burn-cap 0.1
+
+octez-client call increment from owner \
+             --arg "(Left (Right 32))" \
+             --burn-cap 0.1              
